5ac076d934b5aa423bbd5a31e4ceb8d5fd922f1f
raselfrb <raseleee02@gmail.com>
2019-11-12 15:21:30 +0100

    first

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    int i, n;
+    n=1000;
+	printf("Print all values between 0 and 1000, ending with a 7\n");
+	
+    for(i=1; i<=n; i++)
+    {
+        if(i%10 == 7)
+        {
+            printf("%d\n", i);
+        }
+    }
+    return 0;
+}
