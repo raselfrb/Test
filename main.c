9d1ded0295fc4e342a923997d4f6039ec64c66a4
raselfrb <raseleee02@gmail.com>
2020-12-03 09:47:32 +0100

    edited

--- main.c
+++ main.c
@@ -2,16 +2,9 @@
 
 int main(int argc, char **argv)
 {
-   //int i, n; ///change
-    n=1000;
-	printf("Print all values between 0 and 1000, ending with a 7\n");
-	
-    for(i=1; i<=n; i++)
-    {
-        if(i%10 == 7)
-        {
-            printf("%d\n", i);
-        }
-    }
-    return 0;
+    //int i, n; ///change
+    //n=1000;
+    //printf("Print all values between 0 and 1000, ending with a 7\n");
+    g
+    //return 0;
 }
